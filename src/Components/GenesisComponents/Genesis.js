08e8883e7a918b9dab0fd4f08bf087bdd2775a62
Istrate-Andrei <56413983+Kane13581@users.noreply.github.com>
2020-04-30 14:36:42 +0300

    perpetual overlay

---
+++ src/Components/GenesisComponents/Genesis.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Genesis() {
+    return (
+      <div className="GenesisPageStyle">
+       <div className="Project">
+         <div className='P1'>1</div>
+         <div className='P2'>2</div>
+         <div className='P3'>3</div>
+         <div className='P4'>4</div>
+         <div className='P5'>5</div>
+         <div className='P6'>6</div>
+         <div className='P7'>7</div>
+         <div className='P8'>8</div>
+       </div>
+      </div>
+    );
+  }
+  
+  export default Genesis;
